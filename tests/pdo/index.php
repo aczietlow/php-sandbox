abf25f4c6123cca2c8f0735f8e311ce2824b2d5b
Chris Zietlow <zietlow@palantir.net>
2014-01-19 12:46:15 -0500

    Created new database handler

---
+++ tests/pdo/index.php
@@ -0,0 +1,13 @@
+<?php
+//list all available drivers.
+PDO::getAvailableDrivers();
+
+try {
+  // Create a new database handler and connect to the database.
+  $dh = new PDO('mysql:host=127.0.0.1;dbname=test_app', 'root', 'root');
+  $dh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  // Print error from mysql and die.
+  print($e->getMessage());
+  die();
+}
