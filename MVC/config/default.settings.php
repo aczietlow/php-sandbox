1cd499aa7d3b00de097bf7341dc7dd0cbb890607
Chris Zietlow <zietlow@palantir.net>
2014-01-19 16:16:46 -0500

    Added default settings.php as a starting place, and added method in database class to check the credentials provided in the database array in settings.php

---
+++ MVC/config/default.settings.php
@@ -0,0 +1,11 @@
+<?php
+define('URL', 'http://sandbox/php-sandbox/MVC/');
+
+$database = array(
+  'driver' => 'mysql',
+  'host' => '127.0.0.1',
+  'name' => 'sb_mvc',
+  'user' => 'root',
+  'password' => 'root',
+  'port' => '',
+);
