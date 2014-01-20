71aac6d70906ef704edb3a5f4b78d683b13ad08f
Chris Zietlow <zietlow@palantir.net>
2014-01-20 14:01:29 -0500

    Added first array function to demostrate templated style.

--- tests/array_functions/index.php
+++ tests/array_functions/index.php
@@ -1,3 +1,16 @@
 <?php
 
+/**
+ * array_change_key_case(array $array [, int $case = CASE_LOWER ]).
+ * Returns an array with all the keys from $array lowercased or uppercased. Numbered indices are left as is.
+ */
+$input_array = array('FirsT' => 1, 'SeConD' => 2);
+print_r(array_change_key_case($input_array, CASE_UPPER));
 
+/** output
+Array
+(
+    [FIRST] => 1
+    [SECOND] => 4
+)
+*/
