03bb7037dab118ea29af35bdb43f715e26caa412
chris.zietlow <aczietlow@gmail.com>
2014-09-17 13:57:34 -0400

    Added more examples of anonymous functions, including nested anonymous functions.

--- playground/oop/Anonymous.php
+++ playground/oop/Anonymous.php
@@ -1,7 +1,20 @@
 <?php
 echo "<pre>";
 
-// Anonymous functions in procedural php.
+/*
+Anonymous functions are good for few lines code, without polluting the
+namespace such as callbacks.
+*/
+
+// Using create_function to create a lambda function.
+$str = "Hello world.";
+// Create function is depreciated.
+// It was part of a hack until true anonymous functions were introduced.
+$lambda = create_function('$match', 'return "friend";');
+$str = preg_replace_callback('/world/', $lambda, $str);
+echo $str;
+
+echo "\n\n---------------\n\n";
 
 // Using anonymous functions as a php callback.
 echo preg_replace_callback('~-([a-z])~', function ($match) {
@@ -19,7 +32,17 @@ $greet = function($name)
 
 // Outputs Hello World.
 $greet('World');
-// Outputs Hello PHP.
-$greet('PHP');
+
+echo "\n\n---------------\n\n";
+
+function censorString($string, $censor) {
+  return preg_replace_callback('/' . $censor . '/', function ($match) {
+    return str_repeat("*", strlen($match[0]));
+  }, $string);
+}
+
+echo censorString("hello world", "world");
+echo "\n";
+echo censorString("hello world", "hello");
 
 echo "</pre>";
