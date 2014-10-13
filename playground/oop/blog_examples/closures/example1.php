8f77fef847c6166d3254f92bef6d56f8d6bad869
chris.zietlow <aczietlow@gmail.com>
2014-10-12 20:14:29 -0400

    Refactored closure example to use OOP.

--- playground/oop/blog_examples/closures/example1.php
+++ playground/oop/blog_examples/closures/example1.php
@@ -1,17 +1,21 @@
 <?php
 
-function filter($condition, $numbers) {
-  $filtered = array();
-
-
-  // Iterate through all array elements and check the condition.
-  foreach ($numbers as $number) {
-    if ($condition($number)) {
-      $filtered[] = $number;
+class MathsAPI {
+  public function filter($condition, $numbers) {
+    $filtered = array();
+    // Iterate through all array elements and check the condition.
+    foreach ($numbers as $number) {
+      if ($condition($number)) {
+        $filtered[] = $number;
+      }
     }
+    return $filtered;
   }
-  return $filtered;
 }
+
+$math = new MathsAPI();
+
+// Arbitrary numbers are arbitrary.
 $random_numbers = array(34, 56, 1, 5, 67, 123, 4, 55);
 
 // Condition is true is number is a multiple of 2.
@@ -19,7 +23,5 @@ $condition = function($x) {
   return ($x % 2 == 0) ? TRUE : FALSE;
 };
 
-$multiples = filter($condition, $random_numbers);
-
 // Outputs Array ( [0] => 34 [1] => 56 [2] => 4 ).
-print_r($multiples);
+print_r($math->filter($condition, $random_numbers));
